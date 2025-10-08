1b426ad64c6a0531562105cb32292b94e148d8b6
iamllcoolray <llcoolray24@gmail.com>
2025-10-08 00:35:55 -0400

    added more keymappings

--- init.lua
+++ init.lua
@@ -38,9 +38,53 @@ vim.opt.splitbelow = true
 vim.opt.undofile = true
 vim.opt.scrolloff = 8
 
+-- Auto enter insert mode when entering terminal
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+  callback = function()
+    vim.opt_local.number = false
+    vim.opt_local.relativenumber = false
+    vim.cmd("startinsert")
+  end,
+})
+
 -- Basic keymaps
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 vim.keymap.set("n", "<leader>e", vim.cmd.Ex, { desc = "Open file explorer" })
+
+-- Terminal keymaps
+vim.keymap.set("n", "<leader>tt", "<cmd>terminal<CR>", { desc = "Open terminal" })
+vim.keymap.set("n", "<leader>tv", "<cmd>vsplit | terminal<CR>", { desc = "Terminal vertical split" })
+vim.keymap.set("n", "<leader>th", "<cmd>split | terminal<CR>", { desc = "Terminal horizontal split" })
+vim.keymap.set("n", "<leader>tn", "<cmd>tabnew | terminal<CR>", { desc = "Terminal new tab" })
+
+-- Tab navigation
+vim.keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+vim.keymap.set("n", "<leader>tc", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+vim.keymap.set("n", "<leader>tl", "<cmd>tabnext<CR>", { desc = "Next tab" })
+vim.keymap.set("n", "<leader>tp", "<cmd>tabprevious<CR>", { desc = "Previous tab" })
+vim.keymap.set("n", "<tab>", "<cmd>tabnext<CR>", { desc = "Next tab" })
+vim.keymap.set("n", "<S-tab>", "<cmd>tabprevious<CR>", { desc = "Previous tab" })
+
+-- Terminal mode keymaps
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+vim.keymap.set("t", "<C-h>", "<C-\\><C-n><C-w>h", { desc = "Move to left window" })
+vim.keymap.set("t", "<C-j>", "<C-\\><C-n><C-w>j", { desc = "Move to bottom window" })
+vim.keymap.set("t", "<C-k>", "<C-\\><C-n><C-w>k", { desc = "Move to top window" })
+vim.keymap.set("t", "<C-l>", "<C-\\><C-n><C-w>l", { desc = "Move to right window" })
+
+-- Window navigation (normal mode)
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+
+-- Window management
+vim.keymap.set("n", "<leader>wv", "<cmd>vsplit<CR>", { desc = "Split window vertically" })
+vim.keymap.set("n", "<leader>wh", "<cmd>split<CR>", { desc = "Split window horizontally" })
+vim.keymap.set("n", "<leader>wc", "<cmd>close<CR>", { desc = "Close current window" })
+vim.keymap.set("n", "<leader>wo", "<cmd>only<CR>", { desc = "Close all other windows" })
+vim.keymap.set("n", "<leader>w=", "<C-w>=", { desc = "Make splits equal size" })
 
 -- Load plugins from lua/custom/plugins
 require("lazy").setup("custom.plugins", {
